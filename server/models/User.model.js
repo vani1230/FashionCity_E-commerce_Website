32f1cfe9c6a0b9e0fc4790fee14dcdde993e6b80
vani1230 <03vanig@gmail.com>
2026-04-01 17:22:49 +0530

    Important Updates

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
-    name:{type : String, required:true,unique:true},
+    name:{type : String, required:true},
     email:{type : String, required:true,unique:true},
     password:{type : String, required:true},
     role :{type:String,default:'user'},
